28d399eecfbc46d5f70322421862ba7fcc2e5b53
Akanni Emmanuel <akannie33@gmail.com>
2022-10-14 16:50:21 +0100

    task 6

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Success (0)
+ */
+
+int main(void)
+{
+	int first_char;
+
+	for (first_char = 122; first_char >= 97; first_char++)
+	{
+		putchar(first_char);
+	}
+	putchar('\n');
+	return (0);
+}

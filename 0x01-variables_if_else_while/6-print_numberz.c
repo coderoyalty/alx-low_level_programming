1ef057238cdedf3cc93f9b4205b0727842000e8b
Akanni Emmanuel <akannie33@gmail.com>
2022-10-14 16:48:37 +0100

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ * Return: Success (0)
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 48; num < 58; num++)
+	{
+		putchar(num);
+	}
+	putchar('\n');
+	return (0);
+}

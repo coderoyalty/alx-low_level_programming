f79c1cf5c2a5026f2ed89347900392a02b4921eb
Akanni Emmanuel <akannie33@gmail.com>
2022-10-14 16:27:45 +0100

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Success (0)
+ */
+
+int main(void)
+{
+	int first_char;
+
+	for (first_char = 97; first_char <= 122; first_char++)
+	{
+		putchar(first_char);
+	}
+	return (0);
+}

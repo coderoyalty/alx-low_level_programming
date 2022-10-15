803c920d66a356808887d422ea13d77412e154a8
Akanni Emmanuel <akannie33@gmail.com>
2022-10-15 04:04:58 +0100

    advance task 1

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,11 +10,11 @@ int main(void)
 
 	for (i = 48; i <= 56; i++)
 	{
-		for (j = i+1; j <= 57; j++)
+		for (j = i + 1; j <= 57; j++)
 		{
 			putchar(i);
 			putchar(j);
-			if (i != 57)
+			if (i != 56)
 			{
 				putchar(',');
 				putchar(' ');

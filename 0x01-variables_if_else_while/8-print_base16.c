499209cae8209d3241a449ec0a952bed2ad43038
Akanni Emmanuel <akannie33@gmail.com>
2022-10-15 03:40:03 +0100

    task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Success (0)
+ */
+int main(void)
+{
+	int val;
+
+	for (val = 48; val <= 57; val++)
+	{
+		putchar(val);
+	}
+	for (val = 97; val <= 102; val++)
+	{
+		putchar(val);
+	}
+	return (0);
+}

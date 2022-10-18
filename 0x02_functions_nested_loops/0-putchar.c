2a394441d7dec36e8ad403d6b0f65e650856d889
Akanni Emmanuel <akannie33@gmail.com>
2022-10-18 22:57:52 +0100

    task 0

--- 0x02_functions_nested_loops/0-putchar.c
+++ 0x02_functions_nested_loops/0-putchar.c
@@ -1,11 +1,19 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  * main - Entry Point
+ *
  * Return: 0
  */
 int main(void)
 {
-	printf("_putchar");
+	const char* text = "_putchar\n\0";
+	int counter = 0;
+
+	while (text[counter] != '\0')
+	{
+		_putchar(text[counter]);
+		counter++;
+	}
 	return (0);
 }
---
+++ 0x02_functions_nested_loops/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+ * _putchar - writes a char to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ */
+int _putchar(char c);
+#endif

d66d66e8e3da453516df8cc1fefff8e6fea3498a
Akanni Emmanuel <akannie33@gmail.com>
2022-10-19 00:46:03 +0100

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - adds to two number together
+ * @a: first number
+ * @b: second number
+ *
+ * Return: the sum of those numbers
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}

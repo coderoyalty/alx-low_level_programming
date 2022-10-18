346c1616d8feab7def0c4f02c1f001b8e58f127f
Akanni Emmanuel <akannie33@gmail.com>
2022-10-18 23:57:39 +0100

    task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,12 +8,16 @@
  */
 int print_last_digit(int n)
 {
+	int last_digit = 0;
+
 	if (n < 0)
 	{
-		return ((n * -1) % 10);
+		last_digit = (n * -1) % 10;
 	}
 	else
 	{
-		return (n % 10);
+		last_digit = n % 10;
 	}
+	_putchar(last_digit);
+	return (last_digit);
 }

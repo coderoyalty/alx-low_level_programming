4b3a2e15555b2177c62bd39eb9f375ca614c988e
Akanni Emmanuel <akannie33@gmail.com>
2022-10-19 01:08:05 +0100

    task 8

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -20,9 +20,9 @@ int print_last_digit(int n);
 
 void jack_bauer(void);
 
-//void times_table(void);
+/*void times_table(void);*/
 
-//int add(int, int);
+/*int add(int, int);*/
 
-//void print_to_98(int n)
+/*void print_to_98(int n)*/
 #endif

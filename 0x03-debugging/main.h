a836924afd2e62c710e0e2cd276a81453a47602b
Akanni Emmanuel <akannie33@gmail.com>
2022-10-20 13:47:04 +0100

    debugging

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -6,4 +6,8 @@
 void positive_or_negative(int i);
 
 int largest_number(int a, int b, int c);
+
+void print_remaining_days(int month, int days, int year);
+
+int convert_day(int month, int month);
 #endif /* MAIN_H */

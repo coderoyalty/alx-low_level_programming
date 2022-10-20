c20ffff3d2aee539e592a86e5102838a6d8f3b6d
Akanni Emmanuel <akannie33@gmail.com>
2022-10-20 13:49:02 +0100

    debugging

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -13,7 +13,7 @@
 void print_remaining_days(int month, int day, int year)
 {
 	/*checks for if the year is a year or not*/
-	if ((year % 4 == 0) || (year % 100 != 0))
+	if ((year % 4 == 0) || ((year % 400 == 0) && (year % 100 != 0)))
 	{
 		if (month > 2 && day >= 60)
 		{

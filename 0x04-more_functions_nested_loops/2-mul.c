3e576edd500fc415dadc12f876120308f1b9a13c
Akanni Emmanuel <akannie33@gmail.com>
2022-10-20 14:27:30 +0100

    multiply numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,10 @@
+/**
+ * _isupper - multiplies two number @a and @b
+ * @a: first number
+ * @b: second number
+ * Return: 1 on Success otherwise 0
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}

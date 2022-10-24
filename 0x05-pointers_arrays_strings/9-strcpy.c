252d24a73a61a53fc806ce947abccaf3af7aac54
Akanni Emmanuel <akannie33@gmail.com>
2022-10-24 22:22:49 +0100

    strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strcpy - copies @src into @dest
+ * @dest: where to copy to
+ * @src: where to copy from
+ * Return: the pointer to @dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int len = 0;
+
+	while (*(src + len) != '\0')
+	{
+		*(dest + len) = *(src + len);
+		len++;
+	}
+	return (dest);
+}

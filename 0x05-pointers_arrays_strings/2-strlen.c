3d7d8dc7bd24a9a344eeb536b8960af337c2b9cd
Akanni Emmanuel <akannie33@gmail.com>
2022-10-24 11:52:04 +0100

    strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: the string to check
+ * Return: the length of the @s
+ */
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (s[length++] != '\0')
+		;
+	return (length);
+}

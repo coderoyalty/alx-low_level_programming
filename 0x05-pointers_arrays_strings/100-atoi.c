8edbfda58a06c59d41addba6b6925b775ad6b474
Akanni Emmanuel <akannie33@gmail.com>
2022-10-24 22:56:22 +0100

    atoi

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <math.h>
+
+/**
+ * _atoi - converts @s to integer
+ * @s: string
+ * Return: int
+ */
+int _atoi(char *s)
+{
+	int sign = 1;
+	int i = 0;
+	int len = strlen(s) - 1;
+	int val = 0;
+	char c;
+
+	if (s[i] == '+')
+	{
+		len -= 1;
+		i++;
+	}
+	else if (s[i] == '-')
+	{
+		sign = -1;
+		len -= 1;
+		i++;
+	}
+
+	while (*(s + i) != '0')
+	{
+		c = *(s + i);
+
+		if (!((c >= '0') && (c <= '9')))
+			return (-1);
+
+		val += (c - '0') * pow(10, len);
+		len -= 1;
+		i++;
+	}
+
+	return (val * sign);
+}

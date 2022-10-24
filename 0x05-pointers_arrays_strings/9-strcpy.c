dbd7a2755dff02dc21b930f796442bd379190d62
Akanni Emmanuel <akannie33@gmail.com>
2022-10-24 22:30:02 +0100

    strcpy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -10,10 +10,13 @@ char *_strcpy(char *dest, char *src)
 {
 	int len = 0;
 
+	if (
+
 	while (*(src + len) != '\0')
 	{
 		*(dest + len) = *(src + len);
 		len++;
 	}
+	*(dest + len + 1) = '\0';
 	return (dest);
 }

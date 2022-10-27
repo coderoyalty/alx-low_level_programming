38af4f24c6447b3c659fed8e100bf0fb3bccc4ca
Akanni Emmanuel <akannie33@gmail.com>
2022-10-27 23:58:06 +0100

    update main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+void _putchar(char ch);
+
 char *_strcat(char *d, char *s);
 
 char *_strncat(char *d, char *s, int n);
@@ -18,4 +20,6 @@ char *cap_string(char *);
 char *leet(char *str);
 
 char *rot13(char *str);
+
+void print_number(int n);
 #endif

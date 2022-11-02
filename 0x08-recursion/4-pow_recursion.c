59f6d6cc7bb4bc50e236b8cdb1c0144b81e25325
Akanni Emmanuel <akannie33@gmail.com>
2022-11-02 17:27:29 +0100

    solution 4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - finds the x to the power of y
+ * @x: number
+ * @y: number
+ * Return: int
+ */
+int _pow_recursion(int x, int y)
+{
+	if (x < 0)
+	{
+		return (-1);
+	}
+	else if (x == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		return (x * _pow_recursion(x, y - 1));
+	}
+}

3fce689fdc2b552f9e472a329779ab476e265ce3
Akanni Emmanuel <akannie33@gmail.com>
2022-11-04 08:47:32 +0100

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ * argc - command line argument size
+ * argv - command line argument array
+ * Return: int
+ */
+int main(int argc, char **argv)
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}

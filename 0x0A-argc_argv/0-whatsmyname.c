b77e430282408e5affab1a88c64ff26f5677ff61
Akanni Emmanuel <akannie33@gmail.com>
2022-11-04 14:18:18 +0100

    solution 0 correction

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,7 +6,7 @@
  * @argv - command line argument array
  * Return: int
  */
-int main(int __attribute__(((__unused__)) argc, char **argv)
+int main(int __attribute__((__unused__)) argc, char **argv)
 {
 	printf("%s\n", argv[0]);
 	return (0);

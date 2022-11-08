5007a9846a9043cabd2eb77e18f0f145e4d32592
Akanni Emmanuel <akannie33@gmail.com>
2022-11-08 08:34:09 +0100

    handling checker 8 error

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,22 +15,30 @@ char *str_concat(char *s1, char *s2)
 	int total = 0;
 	int i = 0;
 
-	while (*(s1 + len1))
-		len1++;
-
-	while (*(s2 + len2))
-		len2++;
+	if (!s1 && !s2)
+		return (NULL);
+	if (s1 != NULL)
+	{
+		while (*(s1 + len1))
+			len1++;
+	}
+	if (s2 != NULL)
+	{
+		while (*(s2 + len2))
+			len2++;
+	}
 
 	total = len1 + len2;
 
 	mem = (char *)malloc((total + 1) * sizeof(char));
 	if (!mem)
 		return (NULL);
-	for (i = 0; i < len1; i++)
-		mem[i] = s1[i];
-
-	for (i = 0; i < len2; i++)
-		mem[len1 + i] = s2[i];
+	if (s1 != NULL)
+		for (i = 0; i < len1; i++)
+			mem[i] = s1[i];
+	if (s2 != NULL)
+		for (i = 0; i < len2; i++)
+			mem[len1 + i] = s2[i];
 	mem[total] = '\0';
 
 	return (mem);

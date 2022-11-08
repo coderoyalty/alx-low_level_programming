0e3e7e3af36d5a70c78a132184ffcdde98d2bfe2
Akanni Emmanuel <akannie33@gmail.com>
2022-11-08 08:00:21 +0100

    solution 0

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * create_array - creates an array of chrs and initializes it with @c
+ * @size: size to allocate
+ * @c: specific char
+ * Return: pointer to the array
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *dynamic_array = (char *)malloc(size * sizeof(char));
+	int i;
+
+	if (size == 0 || !dynamic_array)
+		return (NULL);
+	for (i = 0; i < size; i++)
+		dynamic_array[i] = c;
+	return (dynamic_array);
+}

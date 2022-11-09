4d9d9611336ed8759989a564c60652e486bee689
Akanni Emmanuel <akannie33@gmail.com>
2022-11-09 05:38:27 +0100

    advance task 1

--- 0x0B-malloc_free/100-argtostr.c
+++ 0x0B-malloc_free/100-argtostr.c
@@ -23,6 +23,8 @@ char *argstostr(int ac, char **av)
 	mem = (char *)malloc(sizeof(char) * (len + 1));
 	if (!mem)
 		return (NULL);
+
+	k = 0;
 	for (i = 0; i < ac; i++)
 	{
 		for (j = 0; av[i][j]; j++)

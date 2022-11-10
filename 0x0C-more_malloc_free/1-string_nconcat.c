22031ed450d816941f6448f69f3fbb3656de934c
Akanni Emmanuel <akannie33@gmail.com>
2022-11-10 20:13:36 +0100

    solution to task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,11 +13,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *mem;
 	unsigned int len1 = 0, len2 = 0, total = 0, i = 0, j = 0;
 
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
-
 	while (s1 && s1[len1])
 		len1++;
 	while (s2 && s2[len2])

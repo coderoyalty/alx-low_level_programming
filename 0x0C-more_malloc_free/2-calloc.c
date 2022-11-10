c42981cda440f1b437f84903e74a8b1ca6ad96cf
Akanni Emmanuel <akannie33@gmail.com>
2022-11-10 20:24:41 +0100

    forgot something

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -12,7 +12,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	char *mem;
 	unsigned int i;
 
-	if (nmemb == 0 || nmemb == 0) 
+	if (nmemb == 0 || size == 0) 
 		return (NULL);
 
 	mem = malloc(nmemb * size);

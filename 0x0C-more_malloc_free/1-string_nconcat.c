fe681466eb695742ca6a759a50bfa7b22453f3c3
Akanni Emmanuel <akannie33@gmail.com>
2022-11-10 19:17:07 +0100

    solution 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -10,29 +10,42 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *mem;
-	int len1 = 0;
-	int len2 = 0;
-	int total = 0;
-	int i = 0;
+	unsigned int len1 = 0;
+	unsigned int len2 = 0;
+	unsigned int total = 0;
+	unsigned int i = 0;
 
 	while (s1 && s1[len1])
 		len1++;
 	while (s2 && s2[len2])
 		len2++;
+
 	if (n < len2)
 		total = len1 + n + 1;
 	else
 		total = len1 + len2 + 1;
+	
 	mem = (char *)malloc(total * sizeof(char));
+	
 	if (!mem)
 		return (NULL);
-	while (s1 && i < len1)
-		mem[i] = s1[i++];
+
+	while (i < len1)
+	{
+		mem[i] = s1[i];
+		i++;
+	}
 	i = 0;
+	
 	if (n > len2)
 		n = len2;
-	while (s2 && i < n)
-		mem[len1 + i] = s2[i++];
+	
+	while (i < n)
+	{
+		mem[len1 + i] = s2[i];
+		i++;
+	}
+
 	mem[total - 1] = '\0';
 
 	return (mem);

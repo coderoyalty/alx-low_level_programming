335519c9eb83e7f15e71e850642e7405d2b61990
Akanni Emmanuel <akannie33@gmail.com>
2022-11-14 23:50:25 +0100

    forgot to include stdlib

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,5 @@
 #include "dog.h"
-
-#define NULL (void *)0
+#include <stdlib.h>
 
 /* make a strlen function */
 
@@ -35,10 +34,11 @@ void _strcpy(char *dest, char *src)
 	int len, i;
 
 	len = _strlen(src);
-	for (i = 0; i <= len; i++)
+	for (i = 0; i < len; i++)
 	{
 		dest[i] = src[i];
 	}
+	dest[i] = '\0';
 }
 
 /**
@@ -52,17 +52,19 @@ void _strcpy(char *dest, char *src)
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *rv = NULL;
+	int len1 = _strlen(name);
+	int len2 = _strlen(owner);
 
 	rv = (dog_t *)malloc(sizeof(dog_t));
 	if (dog == NULL)
 		return (NULL);
-	dog->name = (char *)malloc(sizeof(char) * (_strlen(name) + 1));
+	dog->name = (char *)malloc(sizeof(char) * (len1 + 1));
 	if (dog->name == NULL)
 	{
 		free(dog);
 		return (NULL);
 	}
-	dog->owner = (char *)malloc(sizeof(char) * (_strlen(owner) + 1));
+	dog->owner = (char *)malloc(sizeof(char) * (len2 + 1));
 	if (dog->owner == NULL)
 	{
 		free(dog->name);

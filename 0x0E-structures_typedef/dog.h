778236af613d6839539d9c37b43641245ed16d53
Akanni Emmanuel <akannie33@gmail.com>
2022-11-14 22:24:40 +0100

    poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - structure define dog
+ * @name: name of the dog
+ * @age: dog's age
+ * @owner: name of the dog owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif

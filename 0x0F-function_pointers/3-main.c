c1c52122dade8dc9ee6949454433bab6f411ac37
Akanni Emmanuel <akannie33@gmail.com>
2022-11-16 22:24:28 +0100

    solution to task 3

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,42 @@
+#include "3-calc.h"
+
+/**
+ * main - entry point
+ * @argc: no. of command args
+ * @argv: array containing each command args
+ * Return: int
+ */
+int main(int argc, char **argv)
+{
+	int rhs, lhs;
+	char operand;
+	int (*op_func)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	rhs = atoi(argv[1]);
+	operand = *argv[2];
+	lhs = atoi(argv[3]);
+	op_func = get_op_func(argv[2]);
+
+	if (!op_func)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	/* remember it impossible to divide by 0 */
+	if ((operand == '/' || operand == '%') && lhs == 0)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	printf("%d\n", op_func(rhs, lhs));
+
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "3-calc.h"
+
+/**
+ * op_add - addition operation
+ * @a: first value
+ * @b: second value
+ * Return: int
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtraction operation
+ * @a: first value
+ * @b: second value
+ * Return: int
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplication operation
+ * @a: first value
+ * @b: second value
+ * Return: int
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - division operation
+ * @a: first value
+ * @b: second value
+ * Return: int
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - modulus operation
+ * @a: first value
+ * @b: second value
+ * Return: int
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}

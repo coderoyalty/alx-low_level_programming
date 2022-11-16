ba13b6f4d441969ba767362fc234a167db7168a8
Akanni Emmanuel <akannie33@gmail.com>
2022-11-16 20:30:38 +0100

    What's my name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - function that prints a name
+ * @name: string to print
+ * @f: function pointer
+ * Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,8 @@
+#ifndef FUNCTION_PTR_H
+#define FUNCTION_PTR_H
+
+void _putchar(char c);
+
+void print_name(char *name, void (*f)(char *));
+
+#endif

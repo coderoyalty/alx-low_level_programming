cb945af7892c71601ab4b4ca4e1d961ef91416a1
Akanni Emmanuel <akannie33@gmail.com>
2022-11-17 14:54:41 +0100

    error fixed

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -15,13 +15,13 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	va_start(args, n);
 	for (i = 0; i < n; i++)
 	{
-		str = va_arg(args, char*);
-		if (str != NULL)
+		str = va_arg(args, char *);
+		if ((!separator || (separator && !i)) && str)
 			printf("%s", str);
-		else if (str)
-			printf("(nil)");
-		if (separator && (i + 1) != n)
-			printf(separator);
+		else if (!str)
+			printf("%s(nil)", separator);
+		else
+			printf("%s%s", separator, str);
 	}
 	va_end(args);
 	printf("\n");

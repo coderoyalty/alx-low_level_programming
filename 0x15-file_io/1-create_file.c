92b201d5018bd266cad51452e6758346e1c0f23f
Akanni Emmanuel <akannie33@gmail.com>
2022-12-05 23:58:55 +0100

    Task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,7 +13,9 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (0);
 
-	file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 00600);
+	file = open(filename, O_WRONLY | O_CREAT | O_TRUNC | O_APPEND, 00600);
+	if (file == -1)
+		return (0);
 	len = 0;
 	if (text_content != NULL)
 	{

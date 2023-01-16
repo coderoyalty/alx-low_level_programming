a1b007cde3ed2beb8cd288554739a81f639d880e
Akanni Emmanuel <akannie33@gmail.com>
2023-01-17 00:18:13 +0100

    task 3

---
+++ 0x18-dynamic_libraries/100-operations.h
@@ -0,0 +1,14 @@
+#ifndef _100_OPERATION_H
+#define _100_OPERATION_H
+
+int add(int a, int b);
+
+int sub(int a, int b);
+
+int mul(int a, int b);
+
+int div(int a, int b);
+
+int mod(int a, int b);
+
+#endif

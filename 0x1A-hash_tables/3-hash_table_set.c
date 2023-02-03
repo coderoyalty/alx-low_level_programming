21d4cc253266f8d0084b3b9bbc1ae88d6b4bf835
Royalty <akannie33@gmail.com>
2023-02-03 14:48:32 +0100

    Task 3: betty error fixed

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -45,7 +45,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	ul_int hash_index;
 	hash_node_t *new_node = NULL;
 	hash_node_t *current = NULL;
-	
+
 	if (!ht)
 		return (0);
 	if ((key == NULL) || strlen(key) == 0)

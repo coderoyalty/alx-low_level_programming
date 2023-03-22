12d9e5e50566b2cfdfc6ff52477cdb120aed94d1
Royalty <akannie33@gmail.com>
2023-03-22 23:02:54 +0100

    solution refactoring

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -2,6 +2,25 @@
 """ island_perimeter function """
 
 
+def land_perimeter(grid, i, j):
+    """
+        calculates the perimeter of a land on the
+        grid.
+    """
+    perimeter = 0
+    len_row = len(grid)
+    len_col = len(grid[0])
+    if i <= 0 or grid[i - 1][j] != 1:
+        perimeter += 1
+    if j <= 0 or grid[i][j - 1] != 1:
+        perimeter += 1
+    if i >= len_row - 1 or grid[i + 1][j] != 1:
+        perimeter += 1
+    if j >= len_col - 1 or grid[i][j + 1] != 1:
+        perimeter += 1
+    return perimeter
+
+
 def island_perimeter(grid):
     """
         gets the perimeter for a grid
@@ -15,12 +34,5 @@ def island_perimeter(grid):
         for j in range(len_col):
             value = grid[i][j]
             if value == 1:
-                if i <= 0 or grid[i - 1][j] != 1:
-                    perimeter += 1
-                if j <= 0 or grid[i][j - 1] != 1:
-                    perimeter += 1
-                if i >= len_row - 1 or grid[i + 1][j] != 1:
-                    perimeter += 1
-                if j >= len_col - 1 or grid[i][j + 1] != 1:
-                    perimeter += 1
+                perimeter += land_perimeter(grid, i, j)
     return perimeter

fc1002485df79b0827c0fbfcc2420602adc5241b
Royalty <akannie33@gmail.com>
2023-04-07 23:23:11 +0100

    solution to task 1

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,37 @@
+unsigned int power(unsigned int base, unsigned int exp)
+{
+	if (exp == 0)
+		return (1);
+	return (base * power(base, exp - 1));
+}
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int decimal_val  = 0;
+	unsigned int len = 0;
+	unsigned int i = 0;
+	unsigned int val = 0;
+
+	if (b == (void *)0)
+		return (0);
+	// length of b
+	while (b[i] != '\0')
+	{
+		if (b[i] == '0' || b[i] == '1')
+			i += 1;
+		else
+			return (0);
+	}
+	len = i;
+	i = 0;
+	/** convert from binary to decimal
+	 * using this: (2 ^ (n - 1) * b[i])
+	 */
+	for (i = 0; i < len; i++)
+	{
+		val = b[i] == '0' ? 0 : 1;
+		val = power(2, len - (i + 1)) * val;
+		decimal_val += val;
+	}
+	return (decimal_val);
+}
---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,6 @@
+#include <stdlib.h>
+#include <unistd.h>
+
+int _putchar(char c);
+
+unsigned int binary_to_uint(const char *b);
